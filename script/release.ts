afe47f6995439b36267a6b22a7fd347f3e1c6c41
chen <wszgrcy@gmail.com>
2026-04-23 09:00:02 +0800

    1

---
+++ script/release.ts
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+import { version } from '../package.json';
+function main() {
+  const tagName = `v${version}`;
+  execSync(`git tag -a "${tagName}" -m "Release version ${version}"`, {
+    stdio: 'inherit',
+  });
+
+  execSync(`git push origin "${tagName}"`, {
+    stdio: 'inherit',
+  });
+}
+main();
